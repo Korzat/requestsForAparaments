91c7724bc97afad5a47bc192169c23f44acf3939
Korzat <muhailishe@gmail.com>
2025-04-26 23:17:30 +0300

    Update connection.py

--- connection.py
+++ connection.py
@@ -3,11 +3,11 @@ from func import Parser
 parser = Parser()
 
 all_apartments = []
-nameName = parser.name().values()
-hrefHref = parser.href().values()
+nameName = parser.collection()[0]
+hrefHref = parser.collection()[1]
 
 def add_data():
-    for i, (name, href) in enumerate(zip(nameName, hrefHref), start=1):
+    for i, (name, href) in enumerate(zip(nameName, hrefHref)):
         apartment = {
             f'data_of_apartment №{i}': {  # Используем f-строку для добавления номера
                 "name": name,
@@ -17,4 +17,4 @@ def add_data():
         all_apartments.append(apartment)
 
 add_data()
-# print(all_apartments)
+print(nameName, hrefHref)
